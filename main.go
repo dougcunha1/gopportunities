07996c8c046a22e66a3c00aa466da8504415c647
Douglas <dougcunha@academico.ufs.br>
2024-01-21 19:03:08 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 // /router refere-se ao sub-pacote router
-import "github.com/dougcunha1/gopportunities/router"
+import (
+	"github.com/dougcunha1/gopportunities/config"
+	"github.com/dougcunha1/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	// Initialize router
 	router.Initialize()
 }
