731c31bc1d2e843e0a9a0e7ea06b1acabb14af7e
Douglas <dougcunha@academico.ufs.br>
2024-01-21 23:42:15 -0300

    feat: add update handler logic

--- handler/request.go
+++ handler/request.go
@@ -7,7 +7,6 @@ func errParamisRequired(name, typ string) error {
 }
 
 // Create opening
-
 type CreateOpeningRequest struct {
 	Role     string `json:"role"`
 	Company  string `json:"company"`
@@ -40,4 +39,22 @@ func (r *CreateOpeningRequest) Validate() error {
 		return errParamisRequired("salary", "int64")
 	}
 	return nil
+}
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// If any field is provided, validation is truthy
+	if r.Role != "" || r.Company != "" || r.Link != "" || r.Location != "" || r.Salary > 0 || r.Remote != nil {
+		return nil
+	}
+	// If none of the fields were not provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
 }
