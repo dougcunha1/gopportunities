34c53724f2ffd7c309e1a9347baa0127834a6abb
Douglas <dougcunha@academico.ufs.br>
2024-01-21 21:23:01 -0300

    feat: add createOpening handler logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,12 +1,35 @@
 package handler
 
 import (
+	"github.com/dougcunha1/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Salary:   request.Salary,
+		Link:     request.Link,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opening on database")
+		return
+	}
+
+	sendSuccess(ctx, "create-opening", opening)
 }
--- schemas/opening.go
+++ schemas/opening.go
@@ -2,6 +2,7 @@ package schemas
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type Opening struct {
@@ -12,4 +13,17 @@ type Opening struct {
 	Remote   bool
 	Link     string
 	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID        uint      `json:"id"`
+	CreateAt  time.Time `json:"createAt"`
+	UpdateAt  time.Time `json:"updateAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
 }
